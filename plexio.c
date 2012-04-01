09e4331b0702d64bcac25606745d0c209fafb536
Mathias Rav <rav@cs.au.dk>
2012-04-01 22:22:33 +0200

    Document plexio.c/h

--- plexio.c
+++ plexio.c
@@ -17,23 +17,31 @@ void handle_error(const char * msg) {
   exit(EXIT_FAILURE);
 }
 
+// Restart target program on exit
 int restart;
+// Path to UNIX socket to listen on
 char * socketpath;
+// Number of lines in scrollback buffer
 long int scrollback_size;
+// Name used to start plexio
 char * progname;
 
 static void usage() {
   printf("Usage: %s [-r] [-l scrollback size] <socketpath> <daemon> <daemon args>\n", progname);
 }
 
+// Set global parameter variables and update argc and argv of caller.
 static void parse_args(int * argc, char *** argv) {
+  // Store program name.
   progname = (*argv)[0];
   restart = 0;
   socketpath = 0;
   scrollback_size = 30;
 
+  // Shift program name.
   --*argc, ++*argv;
 
+  // Usage: [-l n] [-r] [--] <socket> <program> [options...]
   while (*argc) {
     char * arg = (*argv)[0];
     if (!strcmp("--", arg)) {
@@ -53,76 +61,115 @@ static void parse_args(int * argc, char *** argv) {
     } else {
       break;
     }
+    // Shift argument.
     --*argc, ++*argv;
   }
 
+  // We require two additional arguments: Socket path and program.
   if (*argc < 2) {
     usage();
     exit(1);
   }
 
+  // Store socket path.
   socketpath = (*argv)[0];
   --*argc, ++*argv;
 }
 
 int main(int argc, char ** argv) {
+  // Parse arguments
   parse_args(&argc, &argv);
 
+  // Start target program
   pid_t child = fork_child(argc, argv);
   register_sigchld_handler();
 
+  // Open UNIX socket
   int sfd = listen_command_socket();
 
+  // Set when we get EOF from the target program
   int eof = 0;
 
+  // List of program clients
   struct list * rfds_l = new_list();
 
+  // Add stdin to clients
   list_insert(rfds_l, 0);
 
+  // Scrollback buffer
   struct lines * scrollback = scrollback_size ? new_lines(scrollback_size) : NULL;
 
   while (restart || !eof) {
     if (eof) {
+      // This is not the first iteration of the loop; restart program.
       waitpid(child, NULL, 0);
       child = fork_child(argc, argv);
       eof = 0;
     }
+
+    // select(1) file descriptors:
     fd_set rfds;
     FD_ZERO(&rfds);
+
+    // Select new socket clients
     FD_SET(sfd, &rfds);
+
+    // Select the stdout/stderr of the target program
     FD_SET(guest_out, &rfds);
+
+    // The highest file descriptor
     int max = (sfd > guest_out) ? sfd : guest_out;
+
+    // Add each client
     for_each_list(rfds_l, l_, i, el) {
       if (el == -1) continue;
       FD_SET(el, &rfds);
       if (el > max) max = el;
     }
+
+    // Wait for something to happen
     int retval = select(max+1, &rfds, NULL, NULL, NULL);
+
     if (retval < 0) {
+      // select might fail due to SIGCHLD; this is handled by the signal handler
       if (got_sigchld) {
 	eof = 1;
 	continue;
       }
+      // otherwise, report error to user
       handle_error("select");
     }
+
+    // Handle each event
     while (retval > 0) {
       if (FD_ISSET(sfd, &rfds)) {
+	// We got a new client
+
 	int cfd = accept_command_client(sfd);
 	int j;
+
+	// Send scrollback buffer to new client
 	if (scrollback != NULL) {
 	  lines_for_each(j, scrollback) {
 	    char * line = scrollback->lines[j];
 	    write_one(line, strlen(line), cfd);
 	  }
 	}
+
+	// Add to list of clients
 	list_insert(rfds_l, cfd);
 	--retval;
       }
+
       if (FD_ISSET(guest_out, &rfds)) {
+	// Target program sent output
+
 	char * buf = read_and_forward_all(guest_out, rfds_l);
 	if (buf == NULL) {
 	  eof = 1;
 	}
+
+	// Go through buffer and add each line to scrollback
 	int line_beginning = 0;
 	int idx = 0;
 	while (buf[idx]) {
@@ -136,12 +183,14 @@ int main(int argc, char ** argv) {
 	FD_CLR(guest_out, &rfds);
 	--retval;
       }
+      // Check if a client sent data to the target program
       for_each_list(rfds_l, l__, j, fd) {
 	if (!retval) break;
 	if (fd == -1) continue;
 	if (!FD_ISSET(fd, &rfds)) continue;
 	--retval;
 	if (!forward(fd, guest_in)) {
+	  // Client disconnected
 	  list_find_and_remove(rfds_l, fd);
 	  close(fd);
 	  break;
@@ -150,8 +199,11 @@ int main(int argc, char ** argv) {
       }
     }
   }
+  // Target program exited and we don't restart it
   close(guest_in);
   close(guest_out);
+
+  // Report program exit to clients and close all sockets
   {
     int status = -1;
     waitpid(child, &status, 0);
--- plexio.h
+++ plexio.h
@@ -7,7 +7,9 @@
 
 void handle_error(const char * msg);
 
+// File descriptors of target program input and output
 extern int guest_in, guest_out;
+// Path to UNIX socket to listen on
 extern char * socketpath;
 
 #endif
